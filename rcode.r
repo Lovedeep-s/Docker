b8b5f91305d214252730a13075dc8f79dbdbdd8e
Lovedeep-s <s.lovedeepparmarpta@gmail.com>
2023-11-23 08:19:12 +0530

    change

--- rcode.r
+++ rcode.r
@@ -1,8 +1,1 @@
-# Load the mtcars dataset
-data(mtcars)
-
-# Fit a linear regression model
-model <- lm(mpg ~ wt, data = mtcars)
-
-# Display summary of the linear regression model
-summary(model)
+print("Hello, World!")
